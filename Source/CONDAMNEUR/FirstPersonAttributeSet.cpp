39833ad9b7d0c139e1cfda7faa90fd4598504141
Anthony.B <anthony.boll@artfx.fr>
2024-04-04 19:52:03 +0200

    [feature] Enemy shooting

--- Source/CONDAMNEUR/FirstPersonAttributeSet.cpp
+++ Source/CONDAMNEUR/FirstPersonAttributeSet.cpp
@@ -6,3 +6,16 @@
 UFirstPersonAttributeSet::UFirstPersonAttributeSet() {
 
 }
+
+void UFirstPersonAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) {
+	NewValue = FMath::Clamp<float>(NewValue, 0, 100);
+}
+
+bool UFirstPersonAttributeSet::PreGameplayEffectExecute(FGameplayEffectModCallbackData& Data) {
+	float ArmorDamage = FMath::Abs(Data.EvaluatedData.Magnitude);
+
+	if (Data.EvaluatedData.Attribute == GetArmorAttribute() && ArmorDamage > GetArmor() && Data.EvaluatedData.Magnitude < 0)
+		Health.SetCurrentValue(Health.GetCurrentValue() - (ArmorDamage - GetArmor()));
+
+	return true;
+}
