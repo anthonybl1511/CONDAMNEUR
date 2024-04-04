1209ae3038d234857319440e61b76923e48cb474
Anthony.B <anthony.boll@artfx.fr>
2024-04-05 00:55:23 +0200

    [feature] Health and Armor items

--- Source/CONDAMNEUR/FirstPersonAttributeSet.cpp
+++ Source/CONDAMNEUR/FirstPersonAttributeSet.cpp
@@ -12,10 +12,23 @@ void UFirstPersonAttributeSet::PreAttributeChange(const FGameplayAttribute& Attr
 }
 
 bool UFirstPersonAttributeSet::PreGameplayEffectExecute(FGameplayEffectModCallbackData& Data) {
-	float ArmorDamage = FMath::Abs(Data.EvaluatedData.Magnitude);
+	float AbsoluteMagnitude = FMath::Abs(Data.EvaluatedData.Magnitude);
 
-	if (Data.EvaluatedData.Attribute == GetArmorAttribute() && ArmorDamage > GetArmor() && Data.EvaluatedData.Magnitude < 0)
-		Health.SetCurrentValue(Health.GetCurrentValue() - (ArmorDamage - GetArmor()));
+	if (Data.EvaluatedData.Magnitude > 0) {
+		if (Data.EvaluatedData.Attribute == GetArmorAttribute() && GetArmor() + AbsoluteMagnitude >= 100) {
+			SetArmor(100);
+			return false;
+		}
+
+		if (Data.EvaluatedData.Attribute == GetHealthAttribute() && GetHealth() + AbsoluteMagnitude >= 100) {
+			SetHealth(100);
+			return false;
+		}
+	}
+	else {
+		if (Data.EvaluatedData.Attribute == GetArmorAttribute() && AbsoluteMagnitude > GetArmor())
+			Health.SetCurrentValue(Health.GetCurrentValue() - (AbsoluteMagnitude - GetArmor()));
+	}
 
 	return true;
 }
